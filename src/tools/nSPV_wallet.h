9cb8cae677e6ad36904d1b49c74971c289eccfa4
jl777 <jameslee777@yahoo.com>
2019-07-21 09:18:28 -1100

    Test

--- src/tools/nSPV_wallet.h
+++ src/tools/nSPV_wallet.h
@@ -399,11 +399,12 @@ bool NSPV_SignTx(btc_tx *mtx,int32_t vini,int64_t utxovalue,cstring *scriptPubKe
         for (i=0; i<(int32_t)siglen; i++)
             fprintf(stderr,"%02x",sig[i]);
         vin = btc_tx_vin(mtx,vini);
-        vin->script_sig = cstr_new_sz(siglen+1);
-        memcpy(vin->script_sig->str,sig,siglen);
-        vin->script_sig->str[siglen] = 1;
-        vin->script_sig->len = siglen+1;
-        fprintf(stderr," sighash %s, sigerr.%d siglen.%d\n",bits256_str(str,sighash),sigerr,vin!=0?(int32_t)vin->script_sig->len:siglen);
+        vin->script_sig = cstr_new_sz(siglen+2);
+        vin->script_sig->str[0] = siglen;
+        memcpy(vin->script_sig->str+1,sig,siglen);
+        vin->script_sig->str[siglen+1] = 1;
+        vin->script_sig->len = siglen+2;
+        fprintf(stderr," sighash %s, sigerr.%d siglen.%d\n",bits256_str(str,sighash),sigerr,vin!=0?(int32_t)vin->script_sig->len:(int32_t)siglen);
     }
     return(true);
 }
