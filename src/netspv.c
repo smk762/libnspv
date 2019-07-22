ae19862a940c9c927bfe3cce5e19623229fa622d
jl777 <jameslee777@yahoo.com>
2019-07-22 01:31:07 -1100

    Syntax

--- src/netspv.c
+++ src/netspv.c
@@ -396,9 +396,10 @@ void btc_net_spv_post_cmd(btc_node *node, btc_p2p_msg_hdr *hdr, struct const_buf
                     revport = ((port >> 8) & 0xff) | ((port & 0xff) << 8);
                     sprintf(ipaddr+strlen(ipaddr),":%u",revport);
                     fprintf(stderr,"%d: %u %llx %s\n",i,timestamp,(long long)services,ipaddr);
-                     = btc_node_new();
+                    node = btc_node_new();
                     if ( btc_node_set_ipport(node, ipaddr) > 0 )
                         btc_node_group_add_node(node->nodegroup,node);
+                    else btc_node_free(node);
                 }
             }
             node->gotaddrs = (uint32_t)time(NULL);
