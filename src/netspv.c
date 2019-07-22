6e802eab6d193e95f6992f7f411235f57cd40e63
jl777 <jameslee777@yahoo.com>
2019-07-22 01:36:39 -1100

    Disable peer addition

--- src/netspv.c
+++ src/netspv.c
@@ -396,10 +396,13 @@ void btc_net_spv_post_cmd(btc_node *node, btc_p2p_msg_hdr *hdr, struct const_buf
                     revport = ((port >> 8) & 0xff) | ((port & 0xff) << 8);
                     sprintf(ipaddr+strlen(ipaddr),":%u",revport);
                     fprintf(stderr,"%d: %u %llx %s\n",i,timestamp,(long long)services,ipaddr);
-                    tmpnode = btc_node_new();
-                    if ( btc_node_set_ipport(tmpnode,ipaddr) > 0 )
-                        btc_node_group_add_node(node->nodegroup,tmpnode);
-                    else btc_node_free(tmpnode);
+                    if ( (0) )
+                    {
+                        tmpnode = btc_node_new();
+                        if ( btc_node_set_ipport(tmpnode,ipaddr) > 0 )
+                            btc_node_group_add_node(node->nodegroup,tmpnode);
+                        else btc_node_free(tmpnode);
+                    }
                 }
             }
             node->gotaddrs = (uint32_t)time(NULL);
