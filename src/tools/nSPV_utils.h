5a4b19446ef7cf3b614f887a61a04f9233673e6f
jl777 <jameslee777@yahoo.com>
2019-07-28 07:06:27 -1100

    +print

--- src/tools/nSPV_utils.h
+++ src/tools/nSPV_utils.h
@@ -900,13 +900,14 @@ int32_t komodo_notaries(btc_spv_client *client,uint8_t pubkeys[64][33],int32_t h
     }
     else
     {
+        fprintf(stderr,"timestamp.%u -> ",timestamp);
         if ( timestamp == 0 )
             timestamp = NSPV_blocktime(client,height);
         else 
             timestamp = timestamp;
         kmd_season = getacseason(timestamp);
     }
-    fprintf(stderr, "timestamp.%d kmd season %d\n",timestamp,(int32_t)kmd_season);
+    fprintf(stderr, "timestamp.%u kmd season %d\n",timestamp,(int32_t)kmd_season);
     if ( kmd_season != 0 )
     {
         if ( didinit[kmd_season-1] == 0 )
