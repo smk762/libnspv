54ec691bb3a2238ff30000d78156a58cfd9213e3
SirSevenG <ottonseven@gmail.com>
2019-07-30 00:49:42 +0700

    [WIP] refactor test_nspv added assert_methods moved nspvlib to framework

---
+++ rpctest/test_framework/nspvlib.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+import requests
+import json
+import ast
+import time
+from .util import assert_true, assert_false, assert_equal
+
+
+def assert_success(result):
+    result_d = type_convert(result)
+    assert_equal(result_d.get('result'), 'success')
+
+
+def assert_noterror(reult):
+    """assert success for a few special cases
+       WIP"""
+    pass
+
+
+def assert_error(result):
+    """ WIP """
+    #result = json.load(result_json)
+    #if reult[1] == "error"
+    #assert_true(result['1'], 'Unexpected result')
+    pass
+
+
+def type_convert(bytez):
+    """WIP"""
+    # r = json.loads(bytes.decode("utf-8"))
+    r = ast.literal_eval(bytez.decode("utf-8"))
+    return r
+
+
+def nspv_broadcast(node_ip, user_pass, rawhex):
+    params = {'userpass': user_pass,
+              'method': 'broadcast'}
+    if rawhex is not False:
+        params.update({'hex': rawhex})
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_getinfo(node_ip, user_pass, height=False):
+    params = {'userpass': user_pass,
+              'method': 'getinfo'}
+    if height is not False:
+        params.update({'height':height})
+    r = requests.post(node_ip, json=params)
+    print(r.json())
+    return r.content
+
+
+def nspv_getnewaddress(node_ip, user_pass):
+    params = {'userpass': user_pass,
+              'method': 'getnewaddress'}
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_getpeerinfo(node_ip, user_pass):
+    params = {'userpass': user_pass,
+              'method': 'getpeerinfo'}
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_hdrsproof(node_ip, user_pass, prevheight, nextheight):
+    params = {'userpass': user_pass,
+              'method': 'hdrsproof'}
+    if prevheight is not False:
+        params.update({'prevheight':prevheight})
+    if nextheight is not False:
+        params.update({'nextheight':nextheight})
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_help(node_ip, user_pass):
+    params = {'userpass': user_pass,
+              'method': 'help'}
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_listtransactions(node_ip, user_pass, address=False, isCC=False, skipcount=False, txfilter=False):
+    params = {'userpass': user_pass,
+              'method': 'listtransactions'}
+    if address is not False:
+        params.update({'address': address})
+    if isCC is not False:
+        params.update({'isCC': isCC})
+    if skipcount is not False:
+        params.update({'skipcount': skipcount})
+    if txfilter is not False:
+        params.update({'filter': txfilter})
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_listunspent(node_ip, user_pass, address=False, isCC=False, skipcount=False, txfilter=False):
+    params = {'userpass': user_pass,
+              'method': 'listunspent'}
+    if address is not False:
+        params.update({'address': address})
+    if isCC is not False:
+        params.update({'isCC': isCC})
+    if skipcount is not False:
+        params.update({'skipcount': skipcount})
+    if txfilter is not False:
+        params.update({'filter': txfilter})
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_login(node_ip, user_pass, wif=False):
+    params = {'userpass': user_pass,
+              'method': 'login'}
+    if wif is not False:
+      params.update({'wif': wif})
+    r = requests.post(node_ip, json=params)
+    print(r.json())
+    return r.content
+
+
+def nspv_logout(node_ip, user_pass):
+    params = {'userpass': user_pass,
+              'method': 'logout'}
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_mempool(node_ip, user_pass):
+    params = {'userpass': user_pass,
+              'method': 'mempool'}
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_notarizations(node_ip, user_pass, height):
+    params = {'userpass': user_pass,
+              'method': 'notarizations'}
+    if height is not False:
+        params.update({'height': height})
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_spend(node_ip, user_pass, address, amount):
+    params = {'userpass': user_pass,
+              'method': 'spend'}
+    if address is not False:
+        params.update({'address': address})
+    if amount is not False:
+        params.update({'amount': amount})
+    r = requests.post(node_ip, json=params)
+    time.sleep(1)
+    return r.content
+
+
+def nspv_spentinfo(node_ip, user_pass, txid, vout):
+    params = {'userpass': user_pass,
+              'method': 'spend'}
+    if txid is not False:
+        params.update({'txid': txid})
+    if vout is not False:
+        params.update({'vout': vout})
+    r = requests.post(node_ip, json=params)
+    time.sleep(1)
+    return r.content
+
+
+def nspv_stop(node_ip, user_pass):
+    params = {'userpass': user_pass,
+              'method': 'stop'}
+    r = requests.post(node_ip, json=params)
+    return r.content
+
+
+def nspv_txproof(node_ip, user_pass, txid, height):
+    params = {'userpass': user_pass,
+              'method': 'txproof'}
+    if txid is not False:
+        params.update({'txid': txid})
+    if height is not False:
+        params.update({'height': height})
+    r = requests.post(node_ip, json=params)
+    return r.content
--- rpctest/test_nspv.py
+++ rpctest/test_nspv.py
@@ -1,55 +1,79 @@
 #!/usr/bin/env python3
 import itertools
 import time
-from nspvlib import *
+import test_framework.nspvlib as tf
 
-local_ip = "http://127.0.0.1:7771"
-userpass = "userpass"
+def main():
+    url = "http://127.0.0.1:7771"
+    userpass = "userpass"
+
+    #  params list format [no value (false), good value, bad value]
+    wif = [False, 'UrJUbSqsb1chYxmQvScdnNhVc2tEJEBDUPMcxCCtgoUYuvyvLKvB', 'thiswontwork']
+    height = [False, 777, 'notnum']
+    prevheight = [False, 765, 'notnum']
+    nextheight = [False, 785, 'notnum']
+    address = [False, 'RYPzyuLXdT9JYn7pemYaX3ytsY3btyaATY', 'not_an_addr']
+    isCCno = [False, 0, 'notnum']
+    isCCyes = [False, 1, 'notnum']
+    skipcount = [False, 2, 'notnum']
+    txfilter = ['not implemented yet']
+    amount = [False, 2, 'notnum']
+    txid = [False, 'f261773a389445100d8dfe4fc0b2d9daeaf90ef6264435e739fbd698624b77d6', 'not_txid']
+    vout = [False, 1,'d']
+    rawhex = [False, '', 'nothex']
+
+    #  methods-to-param dic
+#    nspv_methods = {#'broadcast': [rawhex],
+                    #'getnewaddress': [],                                       V
+                    #'getpeerinfo': [],                                         V
+                    #'hdrsproof': [prevheight,nextheight],                      V
+                    #'help': [],                                                V
+                    #'listtransactions1': [address, isCCno, skipcount],
+                    #'listtransactions2': [address, isCCyes, skipcount],
+                    #'listunspent1': [address, isCCno, skipcount],
+                    #'listunspent2': [address, isCCyes, skipcount],
+                    #'login': [wif], 'logout': [], 'mempool': [],
+                    #'notarizations': [height],                                 V
+                    #'spend': [address, amount],
+                    #'spentinfo': [txid, vout],
+                    #'txproof': [txid, height],
+                    #'stop': []
+#    }
+
+    # help call
+    rpc_call = tf.nspv.help(url, userpass)
+    tf.assert_success(rpc_call)
+
+    # getinfo call
+    rpc_call = tf.nspv_getinfo(url, userpass)
+    tf.assert_success(rpc_call)
+
+    # getpeerinfo call
+    rpc_call = tf.nspv_getpeerinfo(url, userpass)
+    tf.assert_noterror(rpc_call)
+
+    # hdrsproof call
+    rpc_call = tf.nspv_hdrsproof(url, userpass, prevheight[1], nextheight[1])
+    tf.assert_error(rpc_call)
+    rpc_call = tf.nspv_hdrsproof(url, userpass, prevheight[2], nextheight[2])
+    tf.assert_success(rpc_call)
+    rpc_call = tf.nspv_hdrsproof(url, userpass, prevheight[3], nextheight[3])
+    tf.assert_error(rpc_call)
+
+    # notarization call
+    rpc_call = tf.nspv_notarizations(url, userpass, height[1])
+    tf.assert_error(rpc_call)
+    rpc_call = tf.nspv_notarizations(url, userpass, height[2])
+    tf.assert_success(rpc_call)
+    rpc_call = tf.nspv_notarizations(url, userpass, height[3])
+    tf.assert_error(rpc_call)
+
+    # getnewaddress call
+    rpc_call = tf.nspv_getnewaddress()
+    tf.asert_success(rpc_call)
+    rep = tf.type_convert(rpc_call)
+    wif = rep.
 
 
-for method in nspv_methods:
-  param_lists = []
-  for param_list in nspv_methods[method]:
-    param_lists.append(param_list)
-  test_params = list(itertools.product(*param_lists))
-  for x in test_params:
-    csv_row = []
-    print("nspv_"+method+str(x))
-    if method == 'broadcast':
-      resp = nspv_broadcast(local_ip, userpass, *x)
-    elif method == 'getnewaddress':
-      resp = nspv_getnewaddress(local_ip, userpass, *x)
-    elif method == 'getpeerinfo':
-      resp = nspv_getpeerinfo(local_ip, userpass, *x)
-    elif method == 'hdrsproof':
-      resp = nspv_hdrsproof(local_ip, userpass, *x)
-    elif method == 'help':
-      resp = nspv_help(local_ip, userpass, *x)
-    elif method == 'listtransactions1':
-      resp = nspv_listtransactions(local_ip, userpass, *x)
-    elif method == 'listtransactions2':
-      resp = nspv_listtransactions(local_ip, userpass, *x)
-    elif method == 'listunspent1':
-      resp = nspv_listunspent(local_ip, userpass, *x)
-    elif method == 'listunspent2':
-      resp = nspv_listunspent(local_ip, userpass, *x)
-    elif method == 'login':
-      resp = nspv_login(local_ip, userpass, *x)
-    elif method == 'notarizations':
-      resp = nspv_notarizations(local_ip, userpass, *x)
-    elif method == 'spend':
-      resp = nspv_spend(local_ip, userpass, *x)
-    elif method == 'spentinfo':
-      resp = nspv_spentinfo(local_ip, userpass, *x)
-    elif method == 'stop':
-      pass
-      #resp = nspv_stop(local_ip, userpass, *x)
-    elif method == 'txproof':
-      resp = nspv_txproof(local_ip, userpass, *x)
-    try:
-      result = resp.json()
-    except:
-      result = resp.text
-      pass
-    time.sleep(1)
-    print(result)
+if __name__ == "__main__":
+    main()
