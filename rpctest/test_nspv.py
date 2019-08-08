f487b6a6b28173c7d4095352a57c15ec6c93db43
SirSevenG <ottonseven@gmail.com>
2019-08-08 17:34:30 +0700

    cleanup

--- rpctest/test_nspv.py
+++ rpctest/test_nspv.py
@@ -1,59 +1,4 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-import itertools
-from nspvlib import *
-
-local_ip = "http://127.0.0.1:7771"
-userpass = "userpass"
-
-
-for method in nspv_methods:
-  param_lists = []
-  for param_list in nspv_methods[method]:
-    param_lists.append(param_list)
-  test_params = list(itertools.product(*param_lists))
-  for x in test_params:
-    csv_row = []
-    print("nspv_"+method+str(x))
-    if method == 'broadcast':
-      resp = nspv_broadcast(local_ip, userpass, *x)
-    elif method == 'getnewaddress':
-      resp = nspv_getnewaddress(local_ip, userpass, *x)
-    elif method == 'getpeerinfo':
-      resp = nspv_getpeerinfo(local_ip, userpass, *x)
-    elif method == 'hdrsproof':
-      resp = nspv_hdrsproof(local_ip, userpass, *x)
-    elif method == 'help':
-      resp = nspv_help(local_ip, userpass, *x)
-    elif method == 'listtransactions1':
-      resp = nspv_listtransactions(local_ip, userpass, *x)
-    elif method == 'listtransactions2':
-      resp = nspv_listtransactions(local_ip, userpass, *x)
-    elif method == 'listunspent1':
-      resp = nspv_listunspent(local_ip, userpass, *x)
-    elif method == 'listunspent2':
-      resp = nspv_listunspent(local_ip, userpass, *x)
-    elif method == 'login':
-      resp = nspv_login(local_ip, userpass, *x)
-    elif method == 'notarizations':
-      resp = nspv_notarizations(local_ip, userpass, *x)
-    elif method == 'spend':
-      resp = nspv_spend(local_ip, userpass, *x)
-    elif method == 'spentinfo':
-      resp = nspv_spentinfo(local_ip, userpass, *x)
-    elif method == 'stop':
-      resp = nspv_stop(local_ip, userpass, *x)
-    elif method == 'txproof':
-      resp = nspv_txproof(local_ip, userpass, *x)
-    try:
-      result = resp.json()
-    except:
-      result = resp.text
-      pass
-    time.sleep(1)
-    print(result)
-
-=======
 # Copyright (c) 2019 SuperNET developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -397,4 +342,3 @@ def test_autologout():
     rpc_call = call.nspv_spend(addr_send, 0.001)
     call.assert_error(rpc_call)
     print("all tests are finished")
->>>>>>> jl777
