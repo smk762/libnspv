c954b57f1481fbc61619df8b3dbf625e9eef5176
SirSevenG <ottonseven@gmail.com>
2019-08-13 09:20:32 +0700

    fix import

--- rpctest/buildbot.py
+++ rpctest/buildbot.py
@@ -3,10 +3,10 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-from system import subprocess
+from os import subprocess as sub
 import daemon
 import time
 
 with daemon.DaemonContext():
-    subprocess.run("/usr/bin/nohup", "./nspv", "ILN")
+    sub.run(["/usr/bin/nohup", "./nspv", "ILN"])
 time.sleep(5)  # give nspv daemon 5 sec to connect nodes
