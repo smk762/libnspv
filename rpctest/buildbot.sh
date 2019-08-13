605afb0cd0ac08e95e1e2d085d290a4c3edfe29b
SirSevenG <ottonseven@gmail.com>
2019-08-13 22:21:19 +0700

    debug

--- rpctest/buildbot.sh
+++ rpctest/buildbot.sh
@@ -5,6 +5,6 @@
 # 1. run nspv, wait 10 seconds to find good peers
 # 2. run tests, write log
 # 3. wait 4 seconds for tests to start and write log to stdout with tail
-./nspv ILN &>./testnspv.log &
+./nspv HUSH &>./testnspv.log &
 sleep 10  \
 &&  /usr/bin/python3 -m pytest rpctest/test_nspv.py -s | tee ./pytest.log
