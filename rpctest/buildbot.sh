8ed30ece80b33872aa36e77b1fc191eed034a843
SirSevenG <ottonseven@gmail.com>
2019-08-14 19:40:21 +0700

    add trap

--- rpctest/buildbot.sh
+++ rpctest/buildbot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ev
+trap "echo EXIT;  exit" 0
+trap "echo HUP;   exit" 1
+trap "echo CTL-C; exit" 2
+trap "echo QUIT;  exit" 3
+trap "echo ERR;   exit" ERR
 # steps:
 # 1. run nspv, wait 10 seconds to find good peers
 # 2. run tests, write log
