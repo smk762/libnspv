1f1f710e8fe909d60f366ba15a37f877d200451d
jl777 <jameslee777@yahoo.com>
2019-08-15 03:56:11 -1100

    Reverse sign

--- src/tools/nSPV_superlite.h
+++ src/tools/nSPV_superlite.h
@@ -1901,7 +1901,7 @@ char *NSPV_expand_variables(char *bigbuf,char *filestr,char *method,cJSON *argjs
     NSPV_expand_variable(bigbuf,&filestr,"$NETBYTEIN",(char *)replacestr);
     NSPV_expand_variable(bigbuf,&filestr,"$LOGINDISPLAY",NSPV_logintime!=0?"":"none");
     NSPV_expand_variable(bigbuf,&filestr,"$LOGOUTDISPLAY",NSPV_logintime==0?"":"none");
-    sprintf(replacestr,"%d",(int32_t)(time(NULL)-NSPV_logintime)-NSPV_AUTOLOGOUT);
+    sprintf(replacestr,"%d",NSPV_AUTOLOGOUT - (int32_t)(time(NULL)-NSPV_logintime));
     NSPV_expand_variable(bigbuf,&filestr,"$AUTOLOGOUT",replacestr);
 
     // == Error page variable ==
