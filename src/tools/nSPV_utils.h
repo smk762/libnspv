dacc68ce11a01f212323e9fa27a26e430cf17121
jl777 <jameslee777@yahoo.com>
2019-08-15 06:42:19 -1100

    rawseed

--- src/tools/nSPV_utils.h
+++ src/tools/nSPV_utils.h
@@ -46,7 +46,7 @@ bits256 NSPV_seed_to_wif(char *rawseed)
     bits256 privkey; int32_t a,b,c,n=0; char seed[8192],*dest = seed;
     while ( n < sizeof(seed)-1 && (c= rawseed[n]) != 0 && c != 0 && c != '\r' && c != '\n' )
     {
-        if ( c == '%' && (a= src[n+1]) != 0 && (b= src[n+2]) != 0 )
+        if ( c == '%' && (a= rawseed[n+1]) != 0 && (b= rawseed[n+2]) != 0 )
             c = ((unhex(a) << 4) | unhex(b)), n += 2;
         else if ( c == '+' )
             c = ' ';
