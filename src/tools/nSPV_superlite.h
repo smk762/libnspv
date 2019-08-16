657139ca960aff004b50c4c6076280263002f3a5
jl777 <jameslee777@yahoo.com>
2019-08-16 01:32:15 -1100

    Test

--- src/tools/nSPV_superlite.h
+++ src/tools/nSPV_superlite.h
@@ -2020,7 +2020,7 @@ char *NSPV_JSON(cJSON *argjson,char *remoteaddr,uint16_t port,char *filestr,int3
                             {
                                 char srcstr[512],*retstr,*passphrase = "secret"; uint16_t ind; int32_t len = (int32_t)strlen(NSPV_wifstr);
                                 init_hexbytes_noT(srcstr,(uint8_t *)NSPV_wifstr,len);
-                                fprintf(stderr,"login.(%s) -> %s\n",NSPV_wifstr,srcstr);
+                                //fprintf(stderr,"login.(%s) -> %s\n",NSPV_wifstr,srcstr);
                                 ind = 0;
                                 if ( (retstr= LP_jpg("test.jpg","dest.jpg",3,passphrase,srcstr,len*8,&ind)) != 0 )
                                 {
