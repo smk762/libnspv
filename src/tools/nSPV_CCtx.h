f734677bc1a7e8d2f4e75e1e7a935856d27af98b
jl777 <jameslee777@yahoo.com>
2019-08-22 21:54:07 -1100

    Cc tx

--- src/tools/nSPV_CCtx.h
+++ src/tools/nSPV_CCtx.h
@@ -18,6 +18,9 @@
 #define NSPV_CCTX_H
 
 // @blackjok3r and @mihailo implement the CC tx creation functions here
+// instead of a swissarmy knife finalizeCCtx, i think it is better to pass in the specific info needed into a CC signing function. this would eliminate the comparing to all the different possibilities
+// since the CC rpc that creates the tx will know which vins are normal and which ones are CC, and most importantly what type of CC vin it is, it will be much simpler finalize function, though it will mean all the CC rpc calls will have to do more work. that was the rationale behind FinalizeCCtx, but i hear a lot of complaints about the complexity it has become.
+// please make a new way of doing CC tx that wont lead to complaints later. let us start with faucetget
 
 cJSON *NSPV_CC_faucetget()
 {
