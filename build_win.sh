1b14480eb0b3c9ca100d0eee9b10078abd1e7b62
SirSevenG <ottonseven@gmail.com>
2019-08-24 15:56:53 +0700

    enable onetime for wine automations

--- build_win.sh
+++ build_win.sh
@@ -14,7 +14,11 @@ if test -f "$FILE"; then
 fi
 
 cd depends/ && make HOST=$HOST V=1 NO_QT=1
-cd ../
+cd ../src/tools/cryptoconditions
+./autogen.sh
+./configure
+make
+cd ../../..
 
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared
