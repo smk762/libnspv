72de2f9147a671d85fd2bda29cae739364198c7e
SirSevenG <44422309+SirSevenG@users.noreply.github.com>
2019-11-02 01:13:38 +0700

    build_win.sh same func as onetime script

--- build_win.sh
+++ build_win.sh
@@ -3,24 +3,21 @@ export HOST=x86_64-w64-mingw32
 CXX=x86_64-w64-mingw32-g++-posix
 CC=x86_64-w64-mingw32-gcc-posix
 PREFIX="$(pwd)/depends/$HOST"
-FILE=./nspv.exe
 
 set -eu -o pipefail
 
 set -x
 
-if test -f "$FILE"; then
-    make clean
-fi
-
 cd depends/ && make HOST=$HOST V=1 NO_QT=1
-cd ../src/tools/cryptoconditions
-./autogen.sh
-./configure
-make
-cd ../../..
+cd ../
 
+./autogen.sh
+CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared
+
+cd src/tools/cryptoconditions
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared
 CC="${CC} -g " CXX="${CXX} -g " make V=1
 
+cd ../../..
+CC="${CC} -g " CXX="${CXX} -g " make V=1
