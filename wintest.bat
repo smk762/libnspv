c60eed59bb0f44436303923bb177cf8d81ac9a02
SirSevenG <44422309+SirSevenG@users.noreply.github.com>
2019-11-06 21:12:48 +0700

    same windows start for both processes

--- wintest.bat
+++ wintest.bat
@@ -3,4 +3,4 @@ netsh advfirewall firewall add rule name="NSPV TCP Port 12986" dir=in action=all
 netsh advfirewall firewall add rule name="NSPV TCP Port 12986" dir=out action=allow protocol=TCP localport=12986
 start "" /B nspv.exe %CHAIN%  ^1^> log.txt ^2^>^&^1
 PING -n 10 127.0.0.1>nul
-start "" /B /wait python3.exe -m pytest rpctest\test_nspv.py -s
+start "" /B /wait python.exe -m pytest rpctest\test_nspv.py -s
