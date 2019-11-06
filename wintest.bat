bffd8b7cef5abf1bd52fbf1988e3cb54c928b195
SirSevenG <44422309+SirSevenG@users.noreply.github.com>
2019-11-06 20:30:29 +0700

    allow port on win

--- wintest.bat
+++ wintest.bat
@@ -1,2 +1,6 @@
+rem Open TCP Port 12986 inbound and outbound
+netsh advfirewall firewall add rule name="NSPV TCP Port 12986" dir=in action=allow protocol=TCP localport=12986
+netsh advfirewall firewall add rule name="NSPV TCP Port 12986" dir=out action=allow protocol=TCP localport=12986
 start "" nspv.exe %CHAIN%
-start "" /B /wait python.exe -m pytest rpctest\test_nspv.py
+timeout 6
+start "" /B /wait python.exe -m pytest rpctest\test_nspv.py -s
